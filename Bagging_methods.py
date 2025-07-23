66c853bfd02975ab552341416deecf8c0dcb5ffb
Sachin Mugali <sachinmugali555@gmail.com>
2025-07-23 19:20:13 +0530

    Update Bagging_methods.py

--- Bagging_methods.py
+++ Bagging_methods.py
@@ -1,39 +1,47 @@
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split  #to take data as train and spit
-from sklearn.metrics import accuracy_score, classification_report #precision,accuracry score
-from  sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 
-
-#download dataset
-data = pd.read_csv("program6.csv")
-print(data.head())
-print("dataset downloaded")
+#load dataset
+iris=load_iris()
+x=iris.data
+y=iris.target
+feature_names=iris.feature_names
+target_names=iris.target_names
 
 #split the data
-x = data.drop('Outcome',axis = 1)
-y=data['Outcome']
-y = np.array(data.iloc[:,-1])
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.30,random_state=30)
 
-#split data into train and test
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_state=42)
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
+#train the model (Random Forest Classifier)
+clf=RandomForestClassifier(n_estimators=100, random_state=25)
+clf.fit(x_train, y_train)
 
-#initialise naive bayes classify
-nb_classify = GaussianNB()
+#Predictions and report
+y_pred=clf.predict(x_test)
 
-#train model using gaussian
-nb_classify.fit(x_train, y_train)
-y_pred = nb_classify.predict(x_test)
-
-#Evaluate Model
-accuracy=accuracy_score(y_test, y_pred)
-print("accuracy value",accuracy)
-
-print("classification report")
+print("Classification Report")
 print(classification_report(y_test, y_pred))
 
+#Confusion matrix
+cm=confusion_matrix(y_test, y_pred)
+disp=ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
+disp.plot(cmap="Blues")
+plt.title("Confusion matrix")
+plt.show()
 
+#feature Importaces
+importances=clf.feature_importances_
+std=np.std([tree.feature_importances_ for tree in clf.estimators_], axis=0)
+
+forest_importances=pd.Series(importances,index=feature_names)
+fig,ax=plt.subplots()
+forest_importances.plot.bar(yerr=std,ax=ax)
+ax.set_title("feature importances(main decrease in impurity)")
+ax.set_ylabel("Importance")
+plt.tight_layout()
+plt.show()

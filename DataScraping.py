7bab841f7b3d4d2dceea3be8351009c2fa6e031b
Sachin Mugali <sachinmugali555@gmail.com>
2025-07-27 09:48:14 +0530

    Update DataScraping.py

--- DataScraping.py
+++ DataScraping.py
@@ -1,39 +1,23 @@
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+response = request.get("https://books.toScrape.com")
+html_content=response.text
+print(html_content)
 
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC 
-from sklearn.preprocessing import StandardScaler 
-from sklearn.metrics import accuracy_score,classification_report
-import matplotlib.pyplot as plt 
-import seaborn as sns
+if html_content:
+  soup = BeautifulSoup(html_content,'html.parser')
+  print(soup)
+  books = soup.find_all('articles',class='product_pod')
+else:
+  print("no data fetched")
+data=[]
+print("Details of book")
+for book in books:
+  title=book.find('h3').find('a')['title']
+  price=book.find('p', class="price_color").text
+  data.append([title,price'])
 
-#Load dataset
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
-
-#split into training and testing dataset 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42)
-
-#standardize features
-scalar = StandardScaler()
-x_train = scalar.fit_transform(x_train)
-x_test = scalar.transform(x_test)
-
-#train svm model (using rbf kernal by default)
-model = SVC(kernel='rbf',C=1.0,gamma='scale')
-model.fit(x_train,y_train)
-
-#predict and evaluate 
-y_pred = model.predict(x_test)
-print("Accuracy : ",accuracy_score(y_test, y_pred))
-print("Classification Report:",classification_report(y_test, y_pred))
-
-#plot graph
-iris_df = sns.load_dataset('iris')
-sns.pairplot(iris_df,hue='species')
-plt.suptitle('iris dataset feature visualisation',y=1.02)
-plt.show()
-
-
-
+df=pd.DataFrame(data,columns=['title','price'])
+print(df)
+df.to_CSV('books_data.csv',index=false)
